30ca7dd3022cd82e92f9ad9f38c29ce936871858
Scott Stevson <scott@stevson.com>
2023-12-18 10:56:22 -0800

    updating the text in h1; updating the server and port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 """A simple microservice that shows epoch time"""
+import os
 import time
 
 from flask import Flask
@@ -9,8 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def show_epoch_time():
     epoch_time = time.time()
-    return f"<h1>Epoch Time: {epoch_time}</h1>"
+    return f"<h1>The current epoch time is: {epoch_time}</h1>"
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 6738))
+    app.run(host='0.0.0.0', port=port)
