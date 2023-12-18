d109ee9ad34431d720fa4bebd500a5fd52fc4c50
Scott Stevson <scott@stevson.com>
2023-12-18 10:43:37 -0800

    adding a function to display epoch time

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+"""A simple microservice that shows epoch time"""
+import time
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def show_epoch_time():  # put application's code here
-    return 'Hello World!'
+def show_epoch_time():
+    epoch_time = time.time()
+    return f"<h1>Epoch Time: {epoch_time}</h1>"
 
 
 if __name__ == '__main__':
